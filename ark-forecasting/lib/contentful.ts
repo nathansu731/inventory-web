71b54e20e3a12b3c4c4074f7706cff21af452c37
Arosha Sumanaweera <s.arosha@directfn.com>
2025-08-29 21:37:01 +1000

    feat: contentful integration

---
+++ ark-forecasting/lib/contentful.ts
@@ -0,0 +1,10 @@
+import { createClient } from "contentful";
+
+export const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+});
+
+export async function getPages() {
+  return client.getEntries({ content_type: "page" });
+}
